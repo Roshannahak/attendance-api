6ef32aad9bb103248f640b59c703f5cbfad2118d
Roshan nahak <roshannahak1122@gmail.com>
2022-11-25 10:57:42 +0530

    added room name on room model

--- models/room_model.go
+++ models/room_model.go
@@ -7,6 +7,7 @@ import (
 type Room struct {
 	Id             primitive.ObjectID `json:"_id" bson:"_id"`
 	RoomNo         string             `json:"roomno,omitempty"`
+	RoomName       string             `json:"roomname,omitempty"`
 	DepartmentName string             `json:"departmentname,omitempty"`
 	Created        string             `json:"created,omitempty"`
 }
