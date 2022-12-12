2bfd17fc3afaa2a50b2fd003c226cbd5bba8b446
Roshan nahak <roshannahak1122@gmail.com>
2022-12-12 17:08:20 +0530

    ..

--- config/current_ip.go
+++ config/current_ip.go
@@ -1,6 +1,9 @@
 package config
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 func GetCurrentIp() string {
 	ifaces, err := net.Interfaces()
@@ -11,15 +14,17 @@ func GetCurrentIp() string {
 	var ip net.IP
 	for index, interf := range ifaces {
 		addrs, _ := interf.Addrs()
+		fmt.Printf("Full object : %+v \n", interf)
 		for j, addr := range addrs {
-			if index == 2 && j == 0 {
-				switch v := addr.(type) {
-				case *net.IPNet:
-					ip = v.IP
-					break
-				}
+			v := addr.(*net.IPNet)
+			if index == 1 && j == 0 && v != nil {
+				ip = v.IP
+				return ip.String()
+			} else if index == 2 && j == 0 && v != nil {
+				ip = v.IP
 				return ip.String()
 			}
+
 		}
 	}
 	return "localhost"
